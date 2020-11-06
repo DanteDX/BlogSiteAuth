713fba29d0b69f9c169c9f7c05d805b070baab14
dantedxwolf <teacher.martin@gmail.com>
2020-11-06 18:58:04 +0600

    server.js file updated to deploy

--- server.js
+++ server.js
@@ -12,4 +12,15 @@ app.use('/api/logIn',require('./routes/login'));
 app.use('/api/submitBlog',require('./routes/submitBlog'));
 app.use('/api/getAllBlogs',require('./routes/getAllBlogs'));
 
-app.listen(4000,function(){console.log('Listening to port 4000')});
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Server  is runnig on ${PORT}`));
